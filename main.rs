edf47c578a2ca52d477190e330daab243f23b20b
foziaShareen <66553473+foziaShareen@users.noreply.github.com>
2020-10-07 20:05:16 +0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,28 @@
+fn main() {
+   
+    let tuple1=(34,5,6);
+    let (x,y,z)=tuple1;
+    
+    println!("{}",x);
+    println!("{}",y);
+    println!("{}",z);
+    //array examples
+    let a =[2,4,5];
+    //access the array
+    let at_zerinde=a[0];
+        let first=a[1];
+        let second=a[2];
+        //parameterized functions
+      print(8); 
+      let s =3;
+      let d ={
+          let s=6;
+          s+1
+      } ;
+      println!("{}",d);
+}
+fn print(x:i32)
+{ 
+    println!{"the value of x is ={}",x};
+}
+
