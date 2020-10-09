e7d9bbecab28a22b169d89c878246d596f885fb0
foziaShareen <66553473+foziaShareen@users.noreply.github.com>
2020-10-09 10:47:11 +0500

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,28 +1,17 @@
-fn main() {
-   
-    let tuple1=(34,5,6);
-    let (x,y,z)=tuple1;
+struct Rectangle{
+    width:u32,
+    height:u32,
+}
+fn area(rec: &Rectangle)-> u32{
+    rec.width*rec.height
+}
+fn main()
+{
+    let a = Rectangle{
+        width:34,
+        height:23,
+            
+        };      
     
-    println!("{}",x);
-    println!("{}",y);
-    println!("{}",z);
-    //array examples
-    let a =[2,4,5];
-    //access the array
-    let at_zerinde=a[0];
-        let first=a[1];
-        let second=a[2];
-        //parameterized functions
-      print(8); 
-      let s =3;
-      let d ={
-          let s=6;
-          s+1
-      } ;
-      println!("{}",d);
+    println!("the area of {},{} is {}",a.width,a.height,area(&a));
 }
-fn print(x:i32)
-{ 
-    println!{"the value of x is ={}",x};
-}
-
